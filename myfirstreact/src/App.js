c9ca212ab6694107a0f5c52ffebf0f8e5d98a3e6
zoloo <zoloo@LEAP-20s-iMac.local>
2021-05-11 12:09:30 +0800

    5/11 angi

---
+++ myfirstreact/src/App.js
@@ -0,0 +1,50 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+    <div>
+<img src="https://cdns.iconmonstr.com/wp-content/assets/preview/2016/240/iconmonstr-instagram-11.png" alt="Girl in a jacket" width="40" height="40"></img>
+        <span className="divider"></span>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1024px-Instagram_logo.svg.png.png" alt="Girl in a jacket" width="180" height="64"></img>
+       
+
+    </div>
+         <input placeholder="Search"></input>
+      </header>
+      <div>
+        body
+        body
+        body
+        body
+        body
+      </div>
+      <div>
+        body
+        body
+        body
+        body
+        body
+      </div>
+      <div>
+        body
+        body
+        body
+        body
+        body
+      </div>
+      <div>
+        body
+        body
+        body
+        body
+        body
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
